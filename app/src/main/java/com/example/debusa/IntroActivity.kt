90bf133176e6edffc32476f91a09e86c5814e3ce
aruzakii <fahrelardzaky@gmail.com>
2024-06-01 20:33:16 +0700

    pembuatan splash screen dan onboard

---
+++ app/src/main/java/com/example/debusa/IntroActivity.kt
@@ -0,0 +1,25 @@
+package com.example.debusa
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.debusa.databinding.ActivityIntroBinding
+
+class IntroActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/debusa/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.debusa
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.debusa.databinding.ActivitySplashScreenBinding
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, IntroActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },2000)
+    }
+}
